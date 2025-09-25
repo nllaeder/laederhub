c08be6c7b3c1f85ea0af8196c944ebb231e353c7
Nicholas Laeder <nicholas@laederconsulting.com>
2025-09-25 13:42:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9344692773-65be8",
+  "appId": "1:652885727515:web:76a0be0140036232cf1bbe",
+  "apiKey": "AIzaSyBnEJaPSlszxftNTeoeEEDRWxYKaivUSA0",
+  "authDomain": "studio-9344692773-65be8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "652885727515"
+};
